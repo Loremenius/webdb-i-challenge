8a4a892f2da0f4df46a3f417f864689db4b31817
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2019-12-09 20:14:54 -0700

    completed CRUD for accounts. completed all SQL commands requested in queries.sql and completed stretch tasks in queries.sql

--- server.js
+++ server.js
@@ -6,4 +6,72 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/api/accounts', (req, res) => {
+    db.select('*').from('accounts')
+        .then(data=>{
+            res.status(200).json(data);
+        })
+        .catch(error=>{
+            console.log(error);
+            res.status.json({errorMessage: "Error getting accounts"});
+        });
+});
+
+server.get("/api/accounts/:id", (req, res) => {
+    
+    
+    db.select("*").from("accounts").where({ id: req.params.id }).first() 
+      .then(post => {
+        res.status(200).json(post);
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ errorMessage: "Error getting the account" });
+      });
+  });
+
+server.post('/api/accounts', (req, res) => {
+    db.insert(req.body, "id").into('accounts')
+        .then(data=>{
+            res.status(200).json(data);
+        })
+        .catch(error=>{
+            console.log(error);
+            res.status.json({errorMessage: "Error adding account"});
+        });
+});
+
+server.put("/api/accounts/:id", (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+  
+    db("accounts").where({ id }) .update(changes)
+      .then(count => {
+        if (count > 0) {
+          res.status(200).json({ message: `${count} record(s) updated` });
+        } else {
+          res.status(404).json({ message: "Account not found" });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({
+          errorMessage: "Error updating the account"
+        });
+      });
+  });
+
+  server.delete("/api/accounts/:id", (req, res) => {
+    db("accounts").where({ id: req.params.id }).del()
+      .then(count => {
+        res.status(200).json({ message: `${count} record(s) removed` });
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({
+          errorMessage: "Error removing the account"
+        });
+      });
+  });
+
 module.exports = server;
